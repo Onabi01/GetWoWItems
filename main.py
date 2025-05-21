033db00affd3e8d30e8d27e46a69637a699381f3
Onabi01 <clement.attou@gmail.com>
2025-05-21 17:37:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+# main.py
+from scraper import scrape_bis_items
+from lua_writer import write_lua_file
 
+URL = "https://www.icy-veins.com/wow-classic/mage-dps-pve-gear-best-in-slot?area=p6_mage"
+output_path = "data/mage_phase6.lua"
+spec_id = "Mage_Frost_Phase6"
+
+if __name__ == "__main__":
+    bis_data = scrape_bis_items(URL)
+    write_lua_file(spec_id, bis_data, output_path)
